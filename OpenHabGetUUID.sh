bccba024678832f46c5fc6402786dbd169b27648
cytrus77 <cytrus77@gmail.com>
2017-07-22 10:08:00 +0200

    getUuid improvement

--- OpenHabGetUUID.sh
+++ OpenHabGetUUID.sh
@@ -3,7 +3,7 @@
 # OpenHabGetUUID.sh
 #
 
-echo "OpenHab UUID: "
-cat /var/lib/openhab2/uuid
-echo "Openhab secret: "
-cat /var/lib/openhab2/openhabcloud/secret
+uuid=$(</var/lib/openhab2/uuid)
+echo "OpenHab UUID: $uuid"
+secret=$(</var/lib/openhab2/openhabcloud/secret)
+echo "Openhab secret: $secret"
