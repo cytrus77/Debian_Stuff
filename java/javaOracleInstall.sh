773bd5917e4376ee787dd216e3c84afe66f3cf8a
cytrus <cytrus77@gmail.com>
2020-02-17 22:40:48 +0000

    java installer update

--- java/javaOracleInstall.sh
+++ java/javaOracleInstall.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt update
-apt install -y oracle-java8-installer
+apt install -y wget libasound2 libasound2-data
+
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.bell-sw.com/java/13.0.1/bellsoft-jre13.0.1-linux-arm32-vfp-hflt.deb
+dpkg -i bellsoft-jre13.0.1-linux-arm32-vfp-hflt.deb
+apt-get install -f
+dpkg -i bellsoft-jre13.0.1-linux-arm32-vfp-hflt.deb
+
+update-alternatives --install /usr/bin/java java  /usr/lib/jvm/jdk-13.0.1/bin/java 2
+update-alternatives --config java
+
+update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk-13.0.1/bin/jar 2
+update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk-13.0.1/bin/javac 2
+update-alternatives --set jar /usr/lib/jvm/jdk-13.0.1/bin/jar
+update-alternatives --set javac /usr/lib/jvm/jdk-13.0.1/bin/javac
 
 java -version
+
+export J2SDKDIR=/usr/lib/jvm/jdk-13.0.1 >> /etc/profile.d/jdk.sh
+export J2REDIR=/usr/lib/jvm/jdk-13.0.1 >> /etc/profile.d/jdk.sh
+export PATH=$PATH:/usr/lib/jvm/jdk-13.0.1/bin:/usr/lib/jvm/jdk-13.0.1/db/bin >> /etc/profile.d/jdk.sh
+export JAVA_HOME=/usr/lib/jvm/jdk-13.0.1 >> /etc/profile.d/jdk.sh
+export DERBY_HOME=/usr/lib/jvm/jdk-13.0.1/db >> /etc/profile.d/jdk.sh
+
+source /etc/profile.d/jdk.sh
