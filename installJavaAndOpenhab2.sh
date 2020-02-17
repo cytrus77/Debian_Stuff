ea73e72b2f498ac94eeca4a1ab261701dcf384d9
cytrus <cytrus77@gmail.com>
2020-02-17 23:43:04 +0000

    update

--- installJavaAndOpenhab2.sh
+++ installJavaAndOpenhab2.sh
@@ -4,6 +4,12 @@ apt update
 apt upgrade -y
 
 apt install -y default-jre
+
+wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | sudo apt-key add -
+apt-get install apt-transport-https
+echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | sudo tee /etc/apt/sources.list.d/openhab2.list
+
+apt update
 apt install -y openhab2
 apt install -y openhab2-addons
 
